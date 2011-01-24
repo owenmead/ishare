b587c3c752f752defec09117a39cdeb543d7fb26
Owen Mead-Robins <owenmead@gmail.com>
2011-01-23 16:48:46 -0800

    Main page requires user to be logged in

--- ishare/core/views.py
+++ ishare/core/views.py
@@ -1,10 +1,14 @@
 from django.template import RequestContext
 from django.shortcuts import render_to_response
+from django.contrib.auth.decorators import login_required
 
 from ishare.core.models import Item
 
+@login_required
 def index(request):
+    user = request.user
+
     context = {
-        'items' : Item.objects.all(),
+        'items' : Item.objects.filter(owner=user),
     }
     return render_to_response('core/index.html', context, context_instance=RequestContext(request))
