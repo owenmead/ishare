fe2ce05261cf54c9cd312d2ba53a392eb1678e17
Owen Mead-Robins <owenmead@gmail.com>
2011-01-23 16:42:42 -0800

    Now displays some basic stuff

--- ishare/core/views.py
+++ ishare/core/views.py
@@ -1,6 +1,10 @@
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 
+from ishare.core.models import Item
+
 def index(request):
-    context = {}
+    context = {
+        'items' : Item.objects.all(),
+    }
     return render_to_response('core/index.html', context, context_instance=RequestContext(request))
