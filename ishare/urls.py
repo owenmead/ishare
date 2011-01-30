67167714c9cc7cbad39aee5ea53a5e074a90578f
Owen Mead-Robins <owenmead@gmail.com>
2011-01-29 20:37:13 -0800

    Add a basic login form

--- ishare/urls.py
+++ ishare/urls.py
@@ -7,6 +7,8 @@ urlpatterns = patterns('',
     (r'^$', 'core.views.index', {}, 'index'),
     (r'^item/(\d+)/$', 'core.views.item_detail', {}, 'item_detail'),
 
+    (r'^accounts/login/$', 'django.contrib.auth.views.login'),
+
     # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
 )
